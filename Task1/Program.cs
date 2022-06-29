a1d20f9634f2a0970776d483ab328a4b5e8acd3e
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-06-29 22:30:51 +0300

    Updated project

---
+++ Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //ВАЖНО! Перед компиляцией необходимо поместить файл "Text1.txt" на рабочий стол
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "Text1.txt");
+                var text = File.ReadAllText(path);
+
+                //Преобразование текста в массив слов без пробелов и знаков пунктуации
+                var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+                var delimiters = new char[] { ' ', '\n', '\r' };
+                var wordText = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                var wordList = new List<string>(wordText);
+
+                var stopwatch = Stopwatch.StartNew();
+
+                wordList.Insert(wordList.Count / 2, "Новая строка 1");
+
+                Console.WriteLine("Строка добавлена в список");
+                Console.WriteLine($"Вставка в List заняла {stopwatch.Elapsed.TotalMilliseconds} мс\n");
+                Console.ReadKey();
+
+                var wordLinkedList = new LinkedList<string>(wordList);
+
+                var midStr = wordLinkedList.Find("Новая строка 1");
+
+                stopwatch = Stopwatch.StartNew();
+
+                wordLinkedList.AddAfter(midStr, "Новая строка 2");
+
+                Console.WriteLine("Строка добавлена в список");
+                Console.WriteLine($"Вставка в LinkedList заняла {stopwatch.Elapsed.TotalMilliseconds} мс\n");
+                Console.ReadKey();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл не найден!");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка!");
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //ВАЖНО! Перед компиляцией необходимо поместить файл "Text1.txt" на рабочий стол
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "Text1.txt");
+                var text = File.ReadAllText(path);
+
+                //Преобразование текста в массив слов без пробелов и знаков пунктуации
+                var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+                var delimiters = new char[] { ' ', '\n', '\r' };
+                var wordText = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                var wordList = new List<string>(wordText);
+
+                HashSet<string> wordHashSet = new HashSet<string>(wordText);
+
+                Dictionary<string, int> wordDictionary = new Dictionary<string, int>();
+
+                Console.WriteLine("Введите минимальную длину слова для поиска:\n");
+
+                string lengthString;
+                int lengthInt = 0;
+
+                do
+                {
+                    lengthString = Console.ReadLine();
+                }
+                while (!int.TryParse(lengthString, out lengthInt) || (lengthInt < 0));
+
+                foreach (string word in wordHashSet)
+                    if (word.Length < lengthInt)
+                        wordHashSet.Remove(word);
+
+                Console.WriteLine("\nИдёт подсчёт ...");
+
+                foreach (var w in wordHashSet)
+                {
+                    int count = 0;
+
+                    foreach (var ww in wordText)
+                    {
+                        if (w == ww)
+                            count++;
+                    }
+
+                    wordDictionary.Add(w, count);
+                }
+
+                var wordArray = wordDictionary.ToArray();
+
+                for (int i = 0; i < wordArray.Length; i++)
+                {
+                    for (int k = i; k < wordArray.Length; k++)
+                    {
+                        if (wordArray[i].Value < wordArray[k].Value)
+                            (wordArray[i], wordArray[k]) = (wordArray[k], wordArray[i]);
+                    }
+                }
+
+                Console.WriteLine("\nТоп-10 слов текста:\n");
+
+                for (int i = 0; i < 10; i++)
+                    Console.WriteLine($"{i + 1}. {wordArray[i].Key} ({wordArray[i].Value})");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл не найден!");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка!");
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
